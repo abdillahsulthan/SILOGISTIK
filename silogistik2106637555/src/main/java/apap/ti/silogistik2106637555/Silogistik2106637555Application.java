b73ef02cdba97f267c5c196ed82bd8c32b3bc614
abdillahsulthan <abdillahsulthan12@gmail.com>
2023-09-28 23:08:05 +0700

    feat: base code SILOGISTIK

---
+++ silogistik2106637555/src/main/java/apap/ti/silogistik2106637555/Silogistik2106637555Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106637555;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106637555Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106637555Application.class, args);
+	}
+
+}
---
+++ silogistik2106637555/src/test/java/apap/ti/silogistik2106637555/Silogistik2106637555ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106637555;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Silogistik2106637555ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
